a50d2870dae9552799f64b0c747fa681d0edce12
DeicySR <deicy1219@hotmail.com>
2026-02-21 13:38:14 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paqueteQuintoRepo",                       # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Deicy S R",                             # Tu nombre
+    author_email="deicy1219@hotmail.com",           # Tu correo electrónico
+    url="https://github.com/DeicySR/miQuintoRepo",  # URL del proyecto
+)
